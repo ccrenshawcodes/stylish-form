79036cc84d7e8429e6648f60544a0d4d8e9e71ec
catherine <55335367+ccrenshawcodes@users.noreply.github.com>
2025-02-05 17:51:15 -0700

    make select buttons display selected values

--- src/app.js
+++ src/app.js
@@ -1,16 +1,37 @@
+const experienceSelect = document.querySelector('.experience-select');
+const expreienceItem = document.querySelector('.experience-item');
+
+const sourceSelect = document.querySelector('.source-select');
+const sourceItem = document.querySelector('.source-item');
+
+function selectItem (display, val) {
+    display.textContent = val;
+}
+
+// expreienceItem.addEventListener('click', (e) => console.log(typeof e.target.firstChild.data));
+
+expreienceItem.addEventListener('click', (e) => {
+    selectItem(experienceSelect, e.target.firstChild.data);
+})
+
+sourceItem.addEventListener('click', (e) => {
+    selectItem(sourceSelect, e.target.firstChild.data);
+})
+
+
 /* TODO:
-
+-Figure out the best way to have multiple pages
 - spin up a "submission success" page
-- spin up a 404 page
 - wire up Submit button functionality
+    - mvp: sends you to a success page
+    - future vers: 
+        - sends you to a success page that's personalized based on what info you input
+        - sends you a personalized email!!!!
 - wire up Select... button functionality
-    - this should replace "Select..." with their selection
-    - and ideally add it to the posted data....... idk how that would
-    work.
+    - selected data should be added to the form data
 
-- better background/copy
+- hide popover when an option is selected
+- image positioning on large screens
 - carousel?
-- color theme toggle
-- footer with github icon etc.
-
+- submit button tooltip to explain what it actually does? 
 */
