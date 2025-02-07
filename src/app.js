1092230a56fb434d5d76db897c411493ef38d5dc
catherine <55335367+ccrenshawcodes@users.noreply.github.com>
2025-02-06 21:13:40 -0700

    js form validation

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ const calendar = document.querySelector('.cally-button');
 const callyPopover = document.querySelector('.cally-popover');
 const callyMonth = document.querySelector('.cally-month');
 
+const validityAlert = document.querySelector('.alert-error');
+const validityErrorDisplay = document.querySelector('.validation-error');
+
 function resetFields () {
     nameInput.value = '';
     emailInput.value = '';
@@ -34,6 +37,7 @@ function selectItem (display, val) {
 
 expreienceItem.addEventListener('click', (e) => {
     selectItem(experienceSelect, e.target.firstChild.data);
+    console.log(e);
     expreienceItem.hidePopover();
 
 })
@@ -50,15 +54,37 @@ callyMonth.addEventListener('click', () => {
 
 //  show/hide main content vs success message
 submitBtn.addEventListener('click', () => {
-    resetFields();
-    successPage.style.cssText = 'display: block;';
-    mainPage.style.cssText = 'display: none;';
+    const fieldsAreValid = validity();
+    if (!fieldsAreValid) {
+        invalidErrorDisplay();
+    } else {
+        resetFields();
+        successPage.style.cssText = 'display: block;';
+        mainPage.style.cssText = 'display: none;';
+    }
 })
 
 resub.addEventListener('click', () => {
     successPage.style.cssText = 'display: none;';
     mainPage.style.cssText = 'display: flex;';
 })
+
+function validity () {
+    const validName = nameInput.checkValidity();
+    const validEmail = emailInput.checkValidity();
+    const validExperience = experienceSelect !== 'Select...' || null;
+
+    if (validName && validEmail && validExperience) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function invalidErrorDisplay () {
+    validityAlert.style.display = 'block';
+    validityErrorDisplay.textContent = 'Please review the fields above and make sure you have a valid name, email, and experience selected.';
+}
 
 
 /* TODO:
@@ -69,11 +95,11 @@ resub.addEventListener('click', () => {
         - sends you a personalized email!!!!
 - wire up Select... button functionality
     - selected data should be added to the form data
-- add form validation 
 - make responsive for mobile
 
 NIT
 
 BUGS
+- technically it is possible to select a blank value for the experience or source
 
 */
